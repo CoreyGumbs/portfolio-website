9188bb08fffef5e2a272795a7341774a6419fa22
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-08 16:47:09 -0400

    [Add] Added new hero image, new welcome message to js file.

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Corey Gumbs", " Founder, Web Developer & Podcast Engineer", "If everyone is thinking alike, then somebody isn't thinking."],
         typeSpeed: 100,
         backDelay: 900,
         // loop
